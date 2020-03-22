bd341285e10c92389a373cf257d93abb0efd07b4
mmezgin <49004855+mmezgin@users.noreply.github.com>
2020-03-22 23:55:43 +0300

    Add files via upload

--- MovieAPI_NodeJS/app.js
+++ MovieAPI_NodeJS/app.js
@@ -13,6 +13,13 @@ app.use('/api/movies', movie);
 app.get("/", (req, res) => {
     res.send("Successful !");
 });
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') == 'development' ? err : {};
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+});
 app.listen(3100, () => {
     console.log("Server is running !");
 });
--- MovieAPI_NodeJS/routes/movie.js
+++ MovieAPI_NodeJS/routes/movie.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie');
-//pulling spesif id
 
-/// pulling data from db
-router.get("/:movie_id", (req, res) => {
+//top 10 listing
+router.get("/top10", (req, res) => {
+    const promise = Movie.find({}).limit(10).sort({ imdb_score: -1 });
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+//delete
+router.delete("/:movie_id", (req, res, next) => {
+    const promise = Movie.findByIdAndRemove(req.params.movie_id);
+    promise.then((movie) => {
+        if (!movie)
+            next('The movie you looking for is not found !');
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+//update
+router.put("/:movie_id", (req, res, next) => {
+    const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, { new: true });
+    promise.then((movie) => {
+        if (!movie)
+            next('The movie you looking for is not found !');
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+
+
+
+/// pulling all data from db
+router.get("/", (req, res) => {
+    const promise = Movie.find({});
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+
+//pulling specific id
+router.get("/:movie_id", (req, res, next) => {
     const promise = Movie.findById(req.params.movie_id);
     promise.then((movie) => {
+        if (!movie)
+            next('The movie you looking for is not found !');
         res.json(movie);
     }).catch((err) => {
         res.json(err);
@@ -27,7 +72,19 @@ router.post("/", (req, res, next) => {
     */
     const promise = movie.save();
     promise.then((data) => {
-        res.json({ status: 1 });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+//between
+router.get("/between/:start_year/:end_year", (req, res) => {
+    const { start_year, end_year } = req.params;
+    const promise = Movie.find({
+        year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) } // gte büyük eşit lte küçük eşit, lt küçük gt büyük
+    });
+    promise.then((data) => {
+        res.json(data);
     }).catch((err) => {
         res.json(err);
     })

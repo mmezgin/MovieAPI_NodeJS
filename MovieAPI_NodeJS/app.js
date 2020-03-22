6ef0da7fdee9d3c3dea7db89f268a9d3b8548171
mmezgin <49004855+mmezgin@users.noreply.github.com>
2020-03-23 02:28:03 +0300

    Add files via upload

--- MovieAPI_NodeJS/app.js
+++ MovieAPI_NodeJS/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const movie = require('./routes/movie');
+const director = require('./routes/director');
 const bodyParser = require("body-parser");
 ///////
 const db = require('./helper/db.js')();
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/movies', movie);
+app.use('/api/directors', director);
 app.get("/", (req, res) => {
     res.send("Successful !");
 });
